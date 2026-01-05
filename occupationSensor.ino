da98a698377a1f0f4a93c812d1abe10f6ac2d447
An33q <152935788+An33q@users.noreply.github.com>
2026-01-05 13:37:39 -0500

    Version 1 (Basic testing)

---
+++ occupationSensor.ino
@@ -0,0 +1,21 @@
+const int sensorPin = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(sensorPin, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int state = digitalRead(sensorPin);
+
+  if(state == HIGH){
+  Serial.println("NOTHING DETECTED"); //NOTHING IN THE WAY
+  }
+
+  if(state == LOW){
+  Serial.println("OBJECT DETECTED"); //SOMETHING IN THE WAY
+  }
+
+}
