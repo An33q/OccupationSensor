a4ef2000840156f3557c690f6dd6913cf4b025a8
An33q <152935788+An33q@users.noreply.github.com>
2026-01-20 00:06:35 -0500

    Completed occupancy detection code
    Refactor occupancy sensor logic to handle two beams A and B for occupancy counting.

--- occupationSensor.ino
+++ occupationSensor.ino
@@ -1,21 +1,63 @@
-const int sensorPin = 2;
+const int A = 2;
+const int B = 3;
+int count = 0;
+int state;
+unsigned long startTime; 
+unsigned long elapsedTime;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(sensorPin, INPUT_PULLUP);
+  pinMode(A, INPUT_PULLUP);
+  pinMode(B, INPUT_PULLUP);
+  state = 0;
+
 }
 
+// 0 = nothing happened, 1 = A broke first, 2 = B broke first
 void loop() {
-  // put your main code here, to run repeatedly:
-  int state = digitalRead(sensorPin);
 
-  if(state == HIGH){
-  Serial.println("NOTHING DETECTED"); //NOTHING IN THE WAY
+  int Astate = digitalRead(A);
+  int Bstate = digitalRead(B);
+  elapsedTime = millis() - startTime;
+
+  if(state == 0){ //initial state
+    if(Astate == LOW){
+      startTime = millis();
+      state = 1;
+      Serial.println("A BEAM BROKEN");
+    }
+
+    else if(Bstate == LOW){
+      startTime = millis();
+      state = 2;
+      Serial.println("B BEAM BROKEN");     
+    }
   }
 
-  if(state == LOW){
-  Serial.println("OBJECT DETECTED"); //SOMETHING IN THE WAY
+  else if(state == 1){ //for entering
+    if(elapsedTime <= 800 && Bstate == LOW){
+      count++;
+      state = 0;
+      Serial.print("Occupancy Count = ");
+      Serial.println(count);
+    }
+    else if(Astate == HIGH && Bstate == HIGH){ //both beams clear
+      state = 0;
+    } 
   }
-//test
+
+  else if(state == 2){ //for exiting
+    if(elapsedTime <= 800 && Astate == LOW){
+      if(count>0) count--;
+      state = 0;
+      Serial.print("Occupancy Count = ");
+      Serial.println(count);
+    }
+    else if(Astate == HIGH && Bstate == HIGH){ //both beams clear
+      state = 0;
+    } 
+  }  
+
+
 }
